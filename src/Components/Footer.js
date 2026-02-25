50ecb4e4de58ed6e31a8df1f8491d10994416046
Pankaj Kumar <c.thor269@gmail.com>
2026-02-25 12:29:07 +0530

    date

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -23,14 +23,7 @@ class Footer extends Component {
               <ul className="social-links">{networks}</ul>
 
               <ul className="copyright">
-                <li>
-                  &copy; Copyright <span id="year"></span>
-                </li>
-
-                <script>
-                  document.getElementById("year").textContent = new
-                  Date().getFullYear();
-                </script>
+              <li>&copy; Copyright {new Date().getFullYear()}</li>
                 <li>
                   Design by{" "}
                   <a title="Styleshout" href="http://www.styleshout.com/">
